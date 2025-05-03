0baf5f81272c02f8e72cafdd5ec5773f9ac24805
tomerIQ713 <114335797+tomerIQ713@users.noreply.github.com>
2025-05-03 14:08:51 +0300

    Add files via upload

---
+++ install_dependencies.bat
@@ -0,0 +1,12 @@
+@echo off
+echo Installing required Python packages...
+
+pip install customtkinter
+pip install CTkMessagebox
+pip install Pillow
+pip install cryptography
+pip install rsa
+
+echo.
+echo All packages installed successfully!
+pause
