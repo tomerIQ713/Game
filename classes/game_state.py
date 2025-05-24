4853e3e5ccef1c3438d54d79d987a9564eacffd2
tomerIQ713 <114335797+tomerIQ713@users.noreply.github.com>
2025-05-24 14:21:09 +0300

    Delete classes directory

--- classes/game_state.py
+++
@@ -1,9 +0,0 @@
-
-class GameState:
-    def __init__(self):
-        self.username = ""
-        self.selected_theme = 0
-        self.themes = ["Default", "White", "Blue"]
-        self.selected_time_format = None
-        self.selected_game_type = None
-        self.friend_name_to_invite = None  # We store the friend's name here if "Play a friend"
