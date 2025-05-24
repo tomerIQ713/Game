cb34ba14223b04703680378bd46e24aa23ca3ef5
tomerIQ713 <114335797+tomerIQ713@users.noreply.github.com>
2025-05-24 14:31:58 +0300

    Update install_dependencies.bat

--- install_dependencies.bat
+++ install_dependencies.bat
@@ -6,6 +6,9 @@ pip install CTkMessagebox
 pip install Pillow
 pip install cryptography
 pip install rsa
+pip install pygame
+pip install chess
+pip install termcolor
 
 echo.
 echo All packages installed successfully!
